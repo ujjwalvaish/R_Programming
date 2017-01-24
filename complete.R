496bc02e3e2b93c24408be04abbabca67c8c30ec
Ujjwal Vaish <ujjwalvaish@outlook.com>
2017-01-24 12:55:41 -0500

    Find the correlation of nitrate and sulfate concentrations in atmosphere in multiple csv files with observations
    3 FIles

---
+++ complete.R
@@ -0,0 +1,18 @@
+complete <- function(directory = "C:\\Users\\ujjwa\\Desktop\\specdata",id){
+  mydataframe <- NULL
+  ids = NULL
+  counts <- NULL
+    for(myId in id){
+      paste(directory,formatC(myId, width=3, flag="0"),sep = "\\")
+      temp <- read.csv(paste(directory,sprintf("%03d.csv", myId),sep = "\\"))
+      good <- complete.cases(temp)
+      ids <- c(ids, myId)
+      counts <- c(counts,sum(good))
+      cbind(mydataframe,x)
+      
+    }
+  mydataframe <- data.frame(id  = ids, nobs = counts)
+  mydataframe
+}
+
+  
